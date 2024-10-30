a86ef30013ea2dc0b3d9bb1ac5a7a2a13cc714e2
Karan <karanmahatocse@gmail.com>
2024-10-30 20:58:47 +0530

    update user

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -4,6 +4,7 @@ import {
   loginDeliveryPartner,
   refreshToken,
 } from "../controllers/auth/auth.js";
+import { updateUser } from "../controllers/tracking/user.js";
 import { verifytoken } from "../middleware/auth.js";
 
 export const authRoutes = async (fastify, options) => {
@@ -11,4 +12,5 @@ export const authRoutes = async (fastify, options) => {
   fastify.post("/delivery/login", loginDeliveryPartner);
   fastify.post("/refresh-token", refreshToken);
   fastify.get("/user", { preHandler: [verifytoken] }, fetchUser);
+  fastify.patch("/user", { preHandler: [verifytoken] }, updateUser);
 };
