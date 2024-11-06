e59e925c5018a94deb1a61eed67cb668d9de62cf
Karan <karanmahatocse@gmail.com>
2024-11-06 16:50:08 +0530

    absolute path implement

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver', // To resolve custom paths
+      {
+        root: ['./src'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@features': './src/features',
+          '@navigation': './src/navigation',
+          '@patches': './src/patches',
+          '@service': './src/service',
+          '@state': './src/state',
+          '@styles': './src/styles',
+          '@types': './src/types',
+          '@utils': './src/utils',
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
