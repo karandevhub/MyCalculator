85937f25502221e3a44035f327868870645b1d53
Karan <karanmahatocse@gmail.com>
2024-11-07 01:31:20 +0530

    request location permission

--- app/src/features/auth/SplashScreen.tsx
+++ app/src/features/auth/SplashScreen.tsx
@@ -1,9 +1,31 @@
-import { View, StyleSheet, Image } from 'react-native';
-import React, { FC } from 'react';
+import { View, StyleSheet, Image, Alert } from 'react-native';
+import React, { FC, useEffect } from 'react';
 import { Colors } from '@utils/Constants';
 import { screenHeight, screenWidth } from '@utils/Scaling';
 import Logo from '@/assets/images/splash_logo.jpeg';
+import Geolocation from '@react-native-community/geolocation';
+
+Geolocation.setRNConfiguration({
+  skipPermissionRequests: false,
+  authorizationLevel: 'always',
+  enableBackgroundLocationUpdates: true,
+  locationProvider: 'auto',
+});
+
 const SplashScreen: FC = () => {
+  useEffect(() => {
+    const fetchUserLoaction = () => {
+      try {
+        Geolocation.requestAuthorization();
+      } catch (error) {
+        Alert.alert(
+          'Sorry we need location service to give you better shopping experience',
+        );
+      }
+    };
+    const timeoutId = setTimeout(fetchUserLoaction, 1000);
+    return () => clearTimeout(timeoutId);
+  }, []);
   return (
     <View style={styles.container}>
       <Image source={Logo} style={styles.logoImage} />
