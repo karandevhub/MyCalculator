656e8c76854166c920ce6251c6dc3b6d54183cab
Karan <karanmahatocse@gmail.com>
2024-11-07 00:56:00 +0530

    splah screen add

--- app/src/features/auth/SplashScreen.tsx
+++ app/src/features/auth/SplashScreen.tsx
@@ -1,28 +1,28 @@
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 import React, { FC } from 'react';
 import { Colors } from '@utils/Constants';
-import { screenHeight } from '@utils/Scaling';
-
+import { screenHeight, screenWidth } from '@utils/Scaling';
+import Logo from '@/assets/images/splash_logo.jpeg';
 const SplashScreen: FC = () => {
-    return (
-        <View style={styles.container}>
-  
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.logoImage} />
+    </View>
+  );
 };
 
 export default SplashScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: Colors.primary,
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    logoImage: {
-        height: screenHeight * 0.7,
-        width: screenHeight * 0.7,
-        resizeMode: 'contain',
-    }
+  container: {
+    backgroundColor: Colors.primary,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoImage: {
+    height: screenHeight * 0.7,
+    width: screenWidth * 0.7,
+    resizeMode: 'contain',
+  },
 });
